772552e6b4e77e05a2e8fc8ed4e35fb6618147da
Carlos Abea <carlosabea91@gmail.com>
2025-11-22 20:18:36 -0600

    Nose lo que hice

--- CustomAgency/src/main/java/org/example/CustomAgency/model/EstadoRegistro.java
+++ CustomAgency/src/main/java/org/example/CustomAgency/model/EstadoRegistro.java
@@ -1,4 +1,10 @@
 package org.example.CustomAgency.model;
 
 public enum EstadoRegistro {
+    ACTIVO,
+    INACTIVO,
+    PAGADO,
+    PENDIENTE,
+    ANULADA
+
 }
--- CustomAgency/src/main/java/org/example/CustomAgency/model/Usuario.java
+++ CustomAgency/src/main/java/org/example/CustomAgency/model/Usuario.java
@@ -1,4 +1,45 @@
 package org.example.CustomAgency.model;
 
-public class Usuario {
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.ReadOnly;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Usuarios")
+@Getter
+@Setter
+
+public class Usuario extends EntidadAuditable {
+
+    @SequenceGenerator(name = "usuarioseq"
+            , sequenceName = "usuarioseq"
+            ,   allocationSize = 1
+            , initialValue = 1)
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE
+            , generator = "facultadseq")
+
+    @Id
+    @Column(name = "id")
+    @ReadOnly
+    private int id;
+
+    @Required
+    private String username;
+
+    @Required
+    private String nombre;
+    private String email;
+
+    private boolean activo = true;
+
+    //Cuando cree la entidad sucursal voy a establecer la relacion ManyToOne aqui
+
+
+
+
 }
